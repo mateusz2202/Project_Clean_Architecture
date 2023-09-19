9447211ba3966b5ac66b5620825de93e203aa564
Mateusz Jóźwik <jmate2202@outlook.com>
2023-09-20 00:48:08 +0200

    dodanie pliku http do testu

--- OperationAPI/Interfaces/IOperationService.cs
+++ OperationAPI/Interfaces/IOperationService.cs
@@ -1,6 +1,9 @@
-﻿namespace OperationAPI.Interfaces;
+﻿using OperationAPI.Entities;
+
+namespace OperationAPI.Interfaces;
 
 public interface IOperationService
 {
+    public Task<IEnumerable<Operation>> GetAll();
     public Task AddOperation(string name, string code);
 }
