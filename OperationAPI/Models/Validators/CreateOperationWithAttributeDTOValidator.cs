9aa817a00dbfcd3ee3f028be2ac5c428ee582e65
Mateusz Jóźwik <jmate2202@outlook.com>
2023-09-22 01:57:03 +0200

    dodanie sprawdzania walidacji dla dodawania operacji z atrybutami

---
+++ OperationAPI/Models/Validators/CreateOperationWithAttributeDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using OperationAPI.Data;
+
+namespace OperationAPI.Models.Validators;
+
+public class CreateOperationWithAttributeDTOValidator : AbstractValidator<CreateOperationWithAttributeDTO>
+{
+    private readonly OperationDbContext _dbContext;
+    public CreateOperationWithAttributeDTOValidator(OperationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+
+        RuleFor(x => x.CreateOperationDTO)
+            .SetValidator(new CreateOperationDTOValidator(dbContext))
+            .WithMessage(x => $"Incorrect CreateOperationDTO");
+    }
+
+}
