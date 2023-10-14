907acdbb654517d034ff1423c39e3a9cb02af6ed
Mateusz Jóźwik <jmate2202@outlook.com>
2023-10-15 00:21:55 +0200

    podniesienie nugetów

--- src/Frontends/Web/Application/Models/Chat/Message.cs
+++ src/Frontends/Web/Application/Models/Chat/Message.cs
@@ -1,9 +1,8 @@
-namespace BlazorHero.CleanArchitecture.Application.Models.Chat
+namespace BlazorHero.CleanArchitecture.Application.Models.Chat;
+
+public class Message
 {
-    public class Message
-    {
-        public string ToUserId { get; set; }
-        public string FromUserId { get; set; }
-        public string MessageText { get; set; }
-    }
+    public string ToUserId { get; set; }
+    public string FromUserId { get; set; }
+    public string MessageText { get; set; }
 }
