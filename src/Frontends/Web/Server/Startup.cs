bf4d1403d69e4ed564b560dfdc5d752ccefeaa08
Mateusz Jóźwik <jmate2202@outlook.com>
2023-10-15 01:01:08 +0200

    podniesineie klienta do .net 7 :)

--- src/Frontends/Web/Server/Startup.cs
+++ src/Frontends/Web/Server/Startup.cs
@@ -5,7 +5,6 @@ using BlazorHero.CleanArchitecture.Server.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -13,73 +12,63 @@ using System.IO;
 using BlazorHero.CleanArchitecture.Server.Managers.Preferences;
 using Microsoft.Extensions.Localization;
 
-namespace BlazorHero.CleanArchitecture.Server
+namespace BlazorHero.CleanArchitecture.Server;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        _configuration = configuration;
+    }
+
+    private readonly IConfiguration _configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddForwarding(_configuration);
+        services.AddLocalization(options =>
         {
-            _configuration = configuration;
-        }
+            options.ResourcesPath = "Resources";
+        });
+        services.AddCurrentUserService();
+        services.AddSerialization();
+        services.AddDatabase(_configuration);
+        services.AddServerStorage(); 
+        services.AddScoped<ServerPreferenceManager>();
+        services.AddServerLocalization();
+        services.AddIdentity();
+        services.AddJwtAuthentication(services.GetApplicationSettings(_configuration));
+        services.AddSignalR();
+        services.AddApplicationLayer();
+        services.AddApplicationServices();   
+        services.AddSharedInfrastructure(_configuration);
+        services.RegisterSwagger();
+        services.AddInfrastructureMappings();         
+        services.AddControllers().AddValidators();
+        services.AddExtendedAttributesValidators();         
+        services.AddRazorPages();            
+        services.AddLazyCache();
+    }
 
-        private readonly IConfiguration _configuration;
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-
-        public void ConfigureServices(IServiceCollection services)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IStringLocalizer<Startup> localizer)
+    {
+        app.UseForwarding(_configuration);
+        app.UseExceptionHandling(env);
+        app.UseHttpsRedirection();
+        app.UseMiddleware<ErrorHandlerMiddleware>();
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+        app.UseStaticFiles(new StaticFileOptions
         {
-            services.AddForwarding(_configuration);
-            services.AddLocalization(options =>
-            {
-                options.ResourcesPath = "Resources";
-            });
-            services.AddCurrentUserService();
-            services.AddSerialization();
-            services.AddDatabase(_configuration);
-            services.AddServerStorage(); //TODO - should implement ServerStorageProvider to work correctly!
-            services.AddScoped<ServerPreferenceManager>();
-            services.AddServerLocalization();
-            services.AddIdentity();
-            services.AddJwtAuthentication(services.GetApplicationSettings(_configuration));
-            services.AddSignalR();
-            services.AddApplicationLayer();
-            services.AddApplicationServices();   
-            services.AddSharedInfrastructure(_configuration);
-            services.RegisterSwagger();
-            services.AddInfrastructureMappings();         
-            services.AddControllers().AddValidators();
-            services.AddExtendedAttributesValidators();         
-            services.AddRazorPages();
-            services.AddApiVersioning(config =>
-            {
-                config.DefaultApiVersion = new ApiVersion(1, 0);
-                config.AssumeDefaultVersionWhenUnspecified = true;
-                config.ReportApiVersions = true;
-            });
-            services.AddLazyCache();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IStringLocalizer<Startup> localizer)
-        {
-            app.UseForwarding(_configuration);
-            app.UseExceptionHandling(env);
-            app.UseHttpsRedirection();
-            app.UseMiddleware<ErrorHandlerMiddleware>();
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Files")),
-                RequestPath = new PathString("/Files")
-            });
-            app.UseRequestLocalizationByCulture();
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();          
-            app.UseEndpoints();
-            app.ConfigureSwagger();
-            app.UseCors("CorsPolicy");        
-        }
+            FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Files")),
+            RequestPath = new PathString("/Files")
+        });
+        app.UseRequestLocalizationByCulture();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();          
+        app.UseEndpoints();
+        app.ConfigureSwagger();
+        app.UseCors("CorsPolicy");        
     }
 }
